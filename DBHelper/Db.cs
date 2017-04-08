0d2ffdcb03a1859da26f891edc5a1549822e363e
Emre AYRILMAZ <ayrilmaz@outlook.com>
2017-04-08 18:04:46 +0300

    Add project files.

---
+++ DBHelper/Db.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace DBHelper
+{
+    public static class Db
+    {
+        static string connectionString;
+
+        [ThreadStatic]
+        static SqlConnection connection;
+        [ThreadStatic]
+        static SqlTransaction transaction;
+
+        static Db()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public static bool OpenConnection()
+        {
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionString);
+            }
+            if (connection.State == ConnectionState.Closed ||
+                connection.State == ConnectionState.Broken)
+            {
+                connection.Open();
+                return true;
+            }
+            return false;
+        }
+
+        static void CloseConnection(bool close)
+        {
+            if (close)
+            {
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            }
+        }
+
+        static void SetParameterToCommand(SqlCommand command, string sql, params object[] parameterValues)
+        {
+            string pattern = @"@\w+";
+            var parameterNamesFromQuery = Regex.Matches(sql, pattern);
+            if (parameterNamesFromQuery.Count != parameterValues.Length)
+                throw new Exception("Parametre eksik veya fazla!");
+
+            for (int i = 0; i < parameterNamesFromQuery.Count; i++)
+            {
+                command.Parameters.AddWithValue(parameterNamesFromQuery[i].Value, parameterValues[i] ?? DBNull.Value);
+            }
+        }
+
+        public static bool BeginTransaction()
+        {
+            if (transaction == null)
+            {
+                transaction = connection.BeginTransaction();
+                return true;
+            }
+            return false;
+        }
+
+        public static void CommitTransaction(bool commit)
+        {
+            if (commit)
+            {
+                transaction.Commit();
+                transaction.Dispose();
+                transaction = null;
+            }
+        }
+
+        public static void RollbackTransaction()
+        {
+            if (transaction != null)
+            {
+                transaction.Rollback();
+                transaction.Dispose();
+                transaction = null;
+            }
+        }
+
+        public static object ExecuteScalar(string sql, params object[] parameterValues)
+        {
+            bool con = OpenConnection();
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                SetParameterToCommand(cmd, sql, parameterValues);
+
+                if (transaction != null)
+                    cmd.Transaction = transaction;
+
+                try
+                {
+                    return cmd.ExecuteScalar();
+                }
+                finally
+                {
+                    CloseConnection(con);
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string sql, params object[] parameterValues)
+        {
+            bool con = OpenConnection();
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                SetParameterToCommand(cmd, sql, parameterValues);
+
+                if (transaction != null)
+                    cmd.Transaction = transaction;
+
+                try
+                {
+                    return cmd.ExecuteNonQuery();
+                }
+                finally
+                {
+                    CloseConnection(con);
+                }
+            }
+        }
+
+        public static IDataReader ExecuteReader(string sql, params object[] parameterValues)
+        {
+            bool con = OpenConnection();
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SetParameterToCommand(cmd, sql, parameterValues);
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public static void FillTable(DataTable dataTable, string sql, params object[] parameterValues)
+        {
+            bool con = OpenConnection();
+            using (SqlDataAdapter dap = new SqlDataAdapter(sql, connection))
+            {
+                dap.SelectCommand.CommandTimeout = 100;
+                SetParameterToCommand(dap.SelectCommand, sql, parameterValues);
+
+                if (transaction != null)
+                    dap.SelectCommand.Transaction = transaction;
+
+                try
+                {
+                    dap.Fill(dataTable);
+                }
+                finally
+                {
+                    CloseConnection(con);
+                }
+            }
+        }
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransactionTest();
+            Console.ReadLine();
+        }
+
+        static int AddProduct(string name, int amount)
+        {
+            return Db.ExecuteNonQuery("insert Product values(@Name,@Amount)", name, amount);
+        }
+
+        static int ProductCount()
+        {
+            return (int)Db.ExecuteScalar("select count(*) from Product");
+        }
+
+        static int ProductAmount(int id)
+        {
+            return (int)Db.ExecuteScalar("select Amount from Product where Id = @Id", id);
+        }
+
+        static List<string> ProductNames()
+        {
+            List<string> list = new List<string>();
+            var reader = Db.ExecuteReader("select Name from Poduct Order By Name");
+            while (reader.Read())
+                list.Add(reader.GetString(0));
+            reader.Close();
+            return list;
+        }
+
+        static DataTable ProductDataTable()
+        {
+            DataTable dt = new DataTable("Product");
+            Db.FillTable(dt, "select * from Product");
+            return dt;
+        }
+
+        static void TransactionTest()
+        {
+            Db.OpenConnection();
+            var bb = Db.BeginTransaction();
+            try
+            {
+                Db.ExecuteNonQuery("delete Product where Id = @Id", 4);
+                Db.ExecuteNonQuery("update Product sed Amount = 10000 where Id = @Id", 5);
+                Db.CommitTransaction(bb);
+            }
+            catch (Exception ex)
+            {
+                Db.RollbackTransaction();
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
